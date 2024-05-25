3d8c4749fb9785d2afb72cf4fca30718053b26f4
kt-kgs <kota@kgs-design.com>
2024-05-25 15:32:39 +0900

    アコーディオン閉じるでスクロール戻す

--- components/content/Accordion.vue
+++ components/content/Accordion.vue
@@ -1,12 +1,39 @@
 <script setup lang="ts">
 const isShow = ref(false)
+const contentRef = ref<HTMLElement>()
+const toggleButton = ref<HTMLButtonElement>()
+
+onMounted(() => {})
+
+function toggle() {
+  isShow.value ? hide() : show()
+}
+
+function show() {
+  isShow.value = true
+}
+
+function hide() {
+  const contentHeight = contentRef.value?.getBoundingClientRect()?.height || 0
+
+  isShow.value = false
+
+  nextTick(() => {
+    scrollTo({
+      top: scrollY - contentHeight,
+    })
+  })
+}
 </script>
 
 <template>
-  <slot class="" v-if="isShow"></slot>
+  <div ref="contentRef">
+    <slot class="" v-if="isShow"></slot>
+  </div>
   <button
-    @click="isShow = !isShow"
+    @click="toggle()"
     class="flex items-center text-xs bg-transparent text-primary-text gap-0.25em"
+    ref="toggleButton"
   >
     <template v-if="!isShow">
       <span class="i-mdi:plus"></span>
